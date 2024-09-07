f7600fffe5f933f90fb919b443c9177e00d80b10
cpm2028 <cpmccardell@loyola.edu>
2024-09-07 13:11:50 -0400

    Add files via upload

---
+++ lab0.py
@@ -0,0 +1,35 @@
+print("")
+print ("CS151 Lab 0")
+print ("")
+print ("My lab partner is: Bella")
+print ("")
+print("Bella was born in Germany")
+print("")
+print("Bella went to nationals for lacrosse")
+print("")
+print("Bella has two siblings")
+print("")
+print("Bella's favorite ice cream flavor is peppermint")
+print("")
+print("Bella has a dog")
+print("")
+print("Colin")
+print("")
+print("Colin did national cybersecurity in high school")
+print("")
+print("Colin has two mini Bernadoodles")
+print("")
+print("Colin went to Loyola Blakefield high school")
+print("")
+print("Colin runs an online business")
+print("")
+print("Colin's favorite flavor is ice cream is cookie dough")
+print("")
+# Programmers: Colin and Bella
+# Course:  CS151, Mr. John
+# Due Date: 9/10/24
+# Lab Assignment: 0
+# Problem Statement:  Output information about the programmers
+# Data In: None, there is no user input in this program
+# Data Out:  The names of the programmers and five statements about each of them
+# Credits: Class
